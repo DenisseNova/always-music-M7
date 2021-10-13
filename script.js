6f8d4d060a3c457c579739b5f0a965afa9a77825
Denisse Nova <denisse.nova.v@gmail.com>
2021-10-13 14:23:04 -0300

    funcion consultar y consultar rut funcionando

--- script.js
+++ script.js
@@ -3,10 +3,52 @@ const {Client} = require ('pg');
 const config = {
  user: 'postgres',
  host: 'localhost',
- database: 'jeans',
+ database: 'always-music',
  password: '',
  port: 5432
 };
 
 const client = new Client(config);
 
+client.connect(err => {
+  if (err) throw err;
+  else{
+    /*ingresar()
+      .then(() => consultar())
+      */
+    consultar()
+  }
+});
+
+async function ingresar(){
+  const res = await client.query(
+    "insert into estudiantes (nombre, rut, curso, nivel) values ('Ana Martinez', '22.473.234-9', 'Tercero', 'A') RETURNING *"
+  );
+  console.log('registro agregado', res.rows[0]);
+  console.log('Cantidad de registros afectados', res.rowCount);
+  console.log('Campos de Resgistros: ', res.fields.map((r) => r.name).join(' - ')
+  );
+  client.end()
+}
+
+async function consultarRut(rut){
+  const res = await client.query(
+    `SELECt * FROM estudiantes WHERE rut='${rut}'`
+  );
+  console.log('Registros:', res.rows)
+}
+
+async function consultar(){
+  const res = await client.query(
+    'SELECt * FROM estudiantes '
+  );
+  console.log('Registros:', res.rows)
+}
+
+
+
+/*
+client.connect()
+  .then(() => consultar())
+  .then(() => client.end());
+  */
